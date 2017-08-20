f3d7dab2e96fd7fa84a226e207cdc85472712300
kristopher tate <kt@connectfree.co.jp>
2017-08-21 07:21:05 +0900

    treeoflife: completed v1 dht implementation;

--- modules/treeoflife/treeoflife.c
+++ modules/treeoflife/treeoflife.c
@@ -31,6 +31,7 @@ struct treeoflife_dhti {
   uint8_t binlen;
   uint8_t binrep[TOL_ROUTE_LENGTH];
   uint8_t everip_addr[EVERIP_ADDRESS_LENGTH];
+  uint8_t public_key[NOISE_PUBLIC_KEY_LEN];
 
   struct tmr tmr;
 };
@@ -76,6 +77,8 @@ struct treeoflife_csock {
   struct hash *peers_addr;
 
   uint8_t my_everip[EVERIP_ADDRESS_LENGTH];
+  uint8_t my_public_key[NOISE_PUBLIC_KEY_LEN];
+
   struct treeoflife_zone zone[TOL_ZONE_COUNT];
 
   uint16_t child_counter;
@@ -97,9 +100,52 @@ static void treeoflife_dhti_tmr_cb(void *data)
   dhti = mem_deref( dhti );
 }
 
+static int treeoflife_dhti_add_or_update( struct treeoflife_zone *zone 
+                                        , const uint8_t everip_addr[EVERIP_ADDRESS_LENGTH]
+                                        , const uint8_t public_key[NOISE_PUBLIC_KEY_LEN]
+                                        , uint8_t binrep[TOL_ROUTE_LENGTH]
+                                        , uint8_t binlen )
+{
+  int err = 0;
+  struct le *le;
+  bool dhti_already_exists = false;
+  struct treeoflife_dhti *dhti;
+
+  LIST_FOREACH(&zone->dhti_all, le) {
+    dhti = le->data;
+    if (!memcmp(dhti->everip_addr, everip_addr, EVERIP_ADDRESS_LENGTH)) {
+      dhti_already_exists = true;
+      break;
+    }
+  }
+
+  if (!dhti_already_exists) {
+    /* create dhti here */
+    dhti = mem_zalloc(sizeof(*dhti), treeoflife_dhti_destructor);
+    if (!dhti) {
+      err = ENOMEM;
+      goto out;
+    }
+    memcpy(dhti->everip_addr, everip_addr, EVERIP_ADDRESS_LENGTH);
+    memcpy(dhti->public_key, public_key, NOISE_PUBLIC_KEY_LEN);
+    list_append(&zone->dhti_all, &dhti->le, dhti);
+  }
+
+  /* update binrep */
+  dhti->binlen = binlen;
+  memcpy(dhti->binrep, binrep, TOL_ROUTE_LENGTH);
+
+  /* update timer here */
+  tmr_start(&dhti->tmr, TOL_DHT_TIMEOUT_MS, treeoflife_dhti_tmr_cb, dhti);
+
+out:
+  return err;
+}
+
 static
 int treeoflife_peer_dht_notify_send( const uint8_t everip_forward[EVERIP_ADDRESS_LENGTH]
                                    , const uint8_t everip_record[EVERIP_ADDRESS_LENGTH]
+                                   , const uint8_t public_key[NOISE_PUBLIC_KEY_LEN]
                                    , uint16_t zoneid
                                    , uint8_t root[EVERIP_ADDRESS_LENGTH]
                                    , uint8_t binrep[TOL_ROUTE_LENGTH]
@@ -120,6 +166,12 @@ int treeoflife_peer_dht_notify_send( const uint8_t everip_forward[EVERIP_ADDRESS
                                , .l=EVERIP_ADDRESS_LENGTH});
 
   odict_entry_add( od_dht
+                 , "pubkey"
+                 , ODICT_STRING
+                 , &(struct pl){ .p=(const char *)public_key
+                               , .l=NOISE_PUBLIC_KEY_LEN});
+
+  odict_entry_add( od_dht
                  , "root"
                  , ODICT_STRING
                  , &(struct pl){ .p=(const char *)root
@@ -132,6 +184,123 @@ int treeoflife_peer_dht_notify_send( const uint8_t everip_forward[EVERIP_ADDRESS
                                , .l=TOL_ROUTE_LENGTH});
 
   odict_entry_add( od_dht, "bl", ODICT_INT, binlen);
+
+  magi_melchior_send( everip_magi_melchior()
+                    , od_dht
+                    , &(struct pl)PL("tree.dht")
+                    , everip_forward
+                    , 1 /* don't mind if we timeout soon */
+                    , false /* is not routable */
+                    , NULL /* do not expect reply */
+                    , NULL );
+
+  od_dht = mem_deref( od_dht );
+
+  return 0;
+}
+
+static
+int treeoflife_peer_dht_aquire_send( const uint8_t everip_forward[EVERIP_ADDRESS_LENGTH]
+                                   , const uint8_t everip_aquire[EVERIP_ADDRESS_LENGTH]
+                                   , uint16_t zoneid
+                                   , uint8_t root[EVERIP_ADDRESS_LENGTH]
+                                   , uint8_t from_binrep[TOL_ROUTE_LENGTH]
+                                   , uint8_t from_binlen )
+{
+  struct odict *od_dht = NULL;
+
+  odict_alloc(&od_dht, 8);
+
+  odict_entry_add( od_dht, "mode", ODICT_STRING, &(struct pl)PL("aquire"));
+
+  odict_entry_add( od_dht, "zoneid", ODICT_INT, zoneid);
+
+  odict_entry_add( od_dht
+                 , "everip"
+                 , ODICT_STRING
+                 , &(struct pl){ .p=(const char *)everip_aquire
+                               , .l=EVERIP_ADDRESS_LENGTH});
+
+  odict_entry_add( od_dht
+                 , "root"
+                 , ODICT_STRING
+                 , &(struct pl){ .p=(const char *)root
+                               , .l=EVERIP_ADDRESS_LENGTH});
+
+  odict_entry_add( od_dht
+                 , "fbr"
+                 , ODICT_STRING
+                 , &(struct pl){ .p=(const char *)from_binrep
+                               , .l=TOL_ROUTE_LENGTH});
+
+  odict_entry_add( od_dht, "fbl", ODICT_INT, from_binlen);
+
+  magi_melchior_send( everip_magi_melchior()
+                    , od_dht
+                    , &(struct pl)PL("tree.dht")
+                    , everip_forward
+                    , 1 /* don't mind if we timeout soon */
+                    , false /* is not routable */
+                    , NULL /* do not expect reply */
+                    , NULL );
+
+  od_dht = mem_deref( od_dht );
+
+  return 0;
+}
+
+static
+int treeoflife_peer_dht_found_send( const uint8_t everip_forward[EVERIP_ADDRESS_LENGTH]
+                                  , const uint8_t everip_aquire[EVERIP_ADDRESS_LENGTH]
+                                  , const uint8_t public_key[NOISE_PUBLIC_KEY_LEN]
+                                  , uint16_t zoneid
+                                  , uint8_t root[EVERIP_ADDRESS_LENGTH]
+                                  , uint8_t from_binrep[TOL_ROUTE_LENGTH]
+                                  , uint8_t from_binlen
+                                  , uint8_t record_binrep[TOL_ROUTE_LENGTH]
+                                  , uint8_t record_binlen )
+{
+  struct odict *od_dht = NULL;
+
+  odict_alloc(&od_dht, 8);
+
+  odict_entry_add( od_dht, "mode", ODICT_STRING, &(struct pl)PL("found"));
+
+  odict_entry_add( od_dht, "zoneid", ODICT_INT, zoneid);
+
+  odict_entry_add( od_dht
+                 , "everip"
+                 , ODICT_STRING
+                 , &(struct pl){ .p=(const char *)everip_aquire
+                               , .l=EVERIP_ADDRESS_LENGTH});
+
+  odict_entry_add( od_dht
+                 , "pubkey"
+                 , ODICT_STRING
+                 , &(struct pl){ .p=(const char *)public_key
+                               , .l=NOISE_PUBLIC_KEY_LEN});
+
+  odict_entry_add( od_dht
+                 , "root"
+                 , ODICT_STRING
+                 , &(struct pl){ .p=(const char *)root
+                               , .l=EVERIP_ADDRESS_LENGTH});
+
+  odict_entry_add( od_dht
+                 , "fbr"
+                 , ODICT_STRING
+                 , &(struct pl){ .p=(const char *)from_binrep
+                               , .l=TOL_ROUTE_LENGTH});
+
+  odict_entry_add( od_dht, "fbl", ODICT_INT, from_binlen);
+
+  odict_entry_add( od_dht
+                 , "br"
+                 , ODICT_STRING
+                 , &(struct pl){ .p=(const char *)record_binrep
+                               , .l=TOL_ROUTE_LENGTH});
+
+  odict_entry_add( od_dht, "bl", ODICT_INT, record_binlen);
 
   magi_melchior_send( everip_magi_melchior()
                     , od_dht
@@ -491,9 +660,6 @@ static int treeoflife_command_cb_dht( struct treeoflife_csock *tol_c
 
   uint8_t *tmp_everip_record;
 
-  uint8_t *tmp_br;  
-  uint16_t tmp_bl;
-
   if (!tol_c || !rpc)
     return EINVAL;
 
@@ -565,10 +731,122 @@ static int treeoflife_command_cb_dht( struct treeoflife_csock *tol_c
 
   /* mode switch */
   switch (ode->u.pl.l) {
+    case 5:
+      /* found */
+      if (!memcmp(ode->u.pl.p, "found", 5))
+      {
+        uint8_t *tmp_br;
+        uint16_t tmp_bl;
+        uint8_t *tmp_fbr;  
+        uint16_t tmp_fbl;
+        uint8_t *tmp_pubkey;
+        uint8_t everip_addr_route[EVERIP_ADDRESS_LENGTH];
+
+        ode = odict_lookup(rpc->in, "pubkey");
+        if (!ode || ode->type != ODICT_STRING) {
+          err = EPROTO;
+          goto out;
+        }
+
+        /* root must be same as everip address */
+        if (ode->u.pl.l != NOISE_PUBLIC_KEY_LEN) {
+          err = EPROTO;
+          goto out;
+        } 
+
+        tmp_pubkey = (uint8_t *)ode->u.pl.p;
+
+        ode = odict_lookup(rpc->in, "br");
+        if (!ode || ode->type != ODICT_STRING) {
+          err = EPROTO;
+          goto out;
+        }
+
+        if (ode->u.pl.l != TOL_ROUTE_LENGTH) {
+          err = EPROTO;
+          goto out;
+        }
+
+        tmp_br = (uint8_t *)ode->u.pl.p;
+
+        ode = odict_lookup(rpc->in, "bl");
+        if (!ode || ode->type != ODICT_INT) {
+          err = EPROTO;
+          goto out;
+        }
+
+        tmp_bl = (uint16_t)ode->u.integer;
+
+        ode = odict_lookup(rpc->in, "fbr");
+        if (!ode || ode->type != ODICT_STRING) {
+          err = EPROTO;
+          goto out;
+        }
+
+        if (ode->u.pl.l != TOL_ROUTE_LENGTH) {
+          err = EPROTO;
+          goto out;
+        }
+
+        tmp_fbr = (uint8_t *)ode->u.pl.p;
+
+        ode = odict_lookup(rpc->in, "fbl");
+        if (!ode || ode->type != ODICT_INT) {
+          err = EPROTO;
+          goto out;
+        }
+
+        tmp_fbl = (uint16_t)ode->u.integer;
+
+        /* search for entry, and if found -- update */
+        err = treeoflife_dhti_add_or_update( zone
+                                           , tmp_everip_record
+                                           , tmp_pubkey
+                                           , tmp_br
+                                           , tmp_bl );
+        if (err)
+          goto out;
+
+        if (treeoflife_everip_for_route( tol_c
+                                       , tmp_fbr
+                                       , everip_addr_route ))
+          goto out;
+
+        treeoflife_peer_dht_found_send( everip_addr_route
+                                      , tmp_everip_record
+                                      , tmp_pubkey
+                                      , tmp_zoneid
+                                      , tmp_rootp
+                                      , tmp_fbr /* from */
+                                      , tmp_fbl /* from */
+                                      , tmp_br /* record */
+                                      , tmp_bl /* record */
+                                      );
+
+      }
+      break;
     case 6:
-      /* notify */
+      /* notify, aquire */
       if (!memcmp(ode->u.pl.p, "notify", 6))
       {
+        uint8_t *tmp_br;
+        uint16_t tmp_bl;
+        uint8_t *tmp_pubkey;
+
+        ode = odict_lookup(rpc->in, "pubkey");
+        if (!ode || ode->type != ODICT_STRING) {
+          err = EPROTO;
+          goto out;
+        }
+
+        /* root must be same as everip address */
+        if (ode->u.pl.l != NOISE_PUBLIC_KEY_LEN) {
+          err = EPROTO;
+          goto out;
+        } 
+
+        tmp_pubkey = (uint8_t *)ode->u.pl.p;
+
         ode = odict_lookup(rpc->in, "br");
         if (!ode || ode->type != ODICT_STRING) {
           err = EPROTO;
@@ -591,37 +869,13 @@ static int treeoflife_command_cb_dht( struct treeoflife_csock *tol_c
         tmp_bl = (uint16_t)ode->u.integer;
 
         /* okay, save it here */
-        {
-          struct le *le;
-          bool dhti_already_exists = false;
-          struct treeoflife_dhti *dhti;
-
-          LIST_FOREACH(&zone->dhti_all, le) {
-            dhti = le->data;
-            if (!memcmp(dhti->everip_addr, tmp_everip_record, EVERIP_ADDRESS_LENGTH)) {
-              dhti_already_exists = true;
-              break;
-            }
-          }
-
-          if (!dhti_already_exists) {
-            /* create dhti here */
-            dhti = mem_zalloc(sizeof(*dhti), treeoflife_dhti_destructor);
-            if (!dhti) {
-              err = ENOMEM;
-              goto out;
-            }
-            memcpy(dhti->everip_addr, tmp_everip_record, EVERIP_ADDRESS_LENGTH);
-            list_append(&zone->dhti_all, &dhti->le, dhti);
-          }
-
-          /* update binrep */
-          dhti->binlen = tmp_bl;
-          memcpy(dhti->binrep, tmp_br, TOL_ROUTE_LENGTH);
-
-          /* update timer here */
-          tmr_start(&dhti->tmr, TOL_DHT_TIMEOUT_MS, treeoflife_dhti_tmr_cb, dhti);
-        }
+        err = treeoflife_dhti_add_or_update( zone 
+                                           , tmp_everip_record
+                                           , tmp_pubkey
+                                           , tmp_br
+                                           , tmp_bl );
+        if (err)
+          goto out;
 
         /* forward! */
         {
@@ -640,6 +894,7 @@ static int treeoflife_command_cb_dht( struct treeoflife_csock *tol_c
 
             (void)treeoflife_peer_dht_notify_send( zone->parent->cp.everip_addr
                                                  , tmp_everip_record
+                                                 , tmp_pubkey
                                                  , tmp_zoneid
                                                  , tmp_rootp
                                                  , tmp_br
@@ -657,6 +912,7 @@ static int treeoflife_command_cb_dht( struct treeoflife_csock *tol_c
               }
               (void)treeoflife_peer_dht_notify_send( peer->cp.everip_addr
                                                    , tmp_everip_record
+                                                   , tmp_pubkey
                                                    , tmp_zoneid
                                                    , tmp_rootp
                                                    , tmp_br
@@ -667,6 +923,71 @@ static int treeoflife_command_cb_dht( struct treeoflife_csock *tol_c
 
         error("DHT NOTIFY!!!\n");
       }
+      else if (!memcmp(ode->u.pl.p, "aquire", 6))
+      {
+        uint8_t *tmp_fbr;  
+        uint16_t tmp_fbl;
+        ode = odict_lookup(rpc->in, "fbr");
+        if (!ode || ode->type != ODICT_STRING) {
+          err = EPROTO;
+          goto out;
+        }
+
+        if (ode->u.pl.l != TOL_ROUTE_LENGTH) {
+          err = EPROTO;
+          goto out;
+        }
+
+        tmp_fbr = (uint8_t *)ode->u.pl.p;
+
+        ode = odict_lookup(rpc->in, "fbl");
+        if (!ode || ode->type != ODICT_INT) {
+          err = EPROTO;
+          goto out;
+        }
+
+        tmp_fbl = (uint16_t)ode->u.integer;
+
+        /* search for record */
+        {
+          struct le *le;
+          uint8_t everip_addr_route[EVERIP_ADDRESS_LENGTH];
+          struct treeoflife_dhti *dhti;
+
+          LIST_FOREACH(&zone->dhti_all, le) {
+            dhti = le->data;
+            if (!memcmp(dhti->everip_addr, tmp_everip_record, EVERIP_ADDRESS_LENGTH)) {
+              /* if we have record, send it back */
+
+              if (treeoflife_everip_for_route( tol_c
+                                             , tmp_fbr
+                                             , everip_addr_route ))
+                goto out;
+
+              treeoflife_peer_dht_found_send( everip_addr_route
+                                            , dhti->everip_addr
+                                            , dhti->public_key
+                                            , tmp_zoneid
+                                            , tmp_rootp
+                                            , tmp_fbr /* from */
+                                            , tmp_fbl /* from */
+                                            , dhti->binrep /* record */
+                                            , dhti->binlen /* record */
+                                            );
+
+              goto out;
+            }
+          }
+        }
+
+        /* if not found, forward on! */
+
+        error("DHT AQUIRE!!!\n");
+      } else {
+        err = EPROTO;
+        goto out;
+      }
+      break;
     default:
       err = EPROTO;
       goto out;
@@ -887,6 +1208,7 @@ static int treeoflife_command_cb_child( struct treeoflife_csock *tol_c
 
     (void)treeoflife_peer_dht_notify_send( zone->parent->cp.everip_addr
                                          , tol_c->my_everip
+                                         , tol_c->my_public_key
                                          , tmp_zoneid
                                          , zone->root
                                          , zone->binrep
@@ -1194,6 +1516,57 @@ static int treeoflife_command_callback( struct magi_melchior_rpc *rpc
   return EPROTO;
 }
 
+static int _conduit_search( const uint8_t everip_addr[EVERIP_ADDRESS_LENGTH]
+                          , void *arg )
+{
+  struct le *le;
+  struct treeoflife_dhti *dhti;
+  struct treeoflife_peer *peer = NULL;
+  struct treeoflife_csock *tol_c = arg;
+  struct treeoflife_zone *zone = &tol_c->zone[0];
+
+  if (!everip_addr || !tol_c)
+    return EINVAL;
+
+  error("treeoflife: _conduit_search !!\n");
+
+  /* is it inside of our dht? */
+  LIST_FOREACH(&zone->dhti_all, le) {
+    dhti = le->data;
+    if (!memcmp(dhti->everip_addr, everip_addr, EVERIP_ADDRESS_LENGTH)) {
+      break;
+    }
+    dhti = NULL;
+  }
+
+  if (dhti) {
+    /* create new peer */
+    _treeoflife_peer_create( &peer
+                           , tol_c
+                           , dhti->everip_addr );
+    if (!peer)
+      return 0;
+
+    /* initiate peer */
+    conduit_peer_initiate( &peer->cp
+                         , tol_c->conduit
+                         , dhti->public_key
+                         , true );
+  }
+
+  if (!zone->parent)
+    return 0;
+
+  treeoflife_peer_dht_aquire_send( zone->parent->cp.everip_addr
+                                 , everip_addr
+                                 , 0
+                                 , zone->root
+                                 , zone->binrep
+                                 , zone->binlen );
+
+  return 0;
+}
+
 static int magi_event_watcher_h( enum MAGI_EVENTDRIVER_WATCH type
                                , void *data
                                , void *arg )
@@ -1244,6 +1617,11 @@ static int module_init(void)
 
   everip_addr_copy(g_tol->my_everip);
 
+  if (!everip_noise())
+    return EINVAL;
+
+  memcpy(g_tol->my_public_key, everip_noise()->si.public, NOISE_PUBLIC_KEY_LEN);
+
   for (int i = 0; i < TOL_ZONE_COUNT; ++i)
   {
     memcpy(g_tol->zone[i].root, g_tol->my_everip, EVERIP_ADDRESS_LENGTH);
@@ -1279,6 +1657,9 @@ static int module_init(void)
                                 , _conduit_debug
                                 , g_tol );
 
+  conduit_register_search_handler( conduit
+                                 , _conduit_search
+                                 , g_tol );
 
   err = magi_eventdriver_handler_register( everip_eventdriver()
                                          , MAGI_EVENTDRIVER_WATCH_E2E
